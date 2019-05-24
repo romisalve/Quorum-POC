67b7e8c3c65892560bf16b0559d402a8357bfe93
romisalve <rsalveraglio@correo.um.edu.uy>
2019-05-24 13:37:07 -0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,10 @@ var CrudAEOUpdateAndDelete = artifacts.require("./CrudAEOUpdateAndDelete.sol");
 
 module.exports = function(deployer){
 
+  deployer.deploy(CrudAEOStructures,{from: "0x1caFCe8fF232515Dcb07eE7901D49cFF8397eF7c"});
+  
   deployer.deploy(CrudAEOUpdateAndDelete,{from: "0x1caFCe8fF232515Dcb07eE7901D49cFF8397eF7c"});
 
-deployer.deploy(CrudAEOUpdateAndDelete,{from: "0x1caFCe8fF232515Dcb07eE7901D49cFF8397eF7c"});
+deployer.deploy(CrudAEOCreateAndRead,{from: "0x1caFCe8fF232515Dcb07eE7901D49cFF8397eF7c"});
 
 };
