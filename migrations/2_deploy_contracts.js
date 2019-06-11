a74841ec76de13e9dfdf7e13dfd9ba936e8a3836
romisalve <rsalveraglio@correo.um.edu.uy>
2019-06-11 15:32:04 -0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,17 @@
+var StructuresAndVariables= artifacts.require("./StructuresAndVariables.sol");
 var CrudAEOStructures= artifacts.require("./CrudAEOStructures.sol");
 var CrudAEOCreateAndRead= artifacts.require("./CrudAEOCreateAndRead.sol");
 var CrudAEOUpdateAndDelete = artifacts.require("./CrudAEOUpdateAndDelete.sol");
 
 module.exports = function(deployer){
 
+  deployer.deploy(StructuresAndVariables);
+  deployer.link(StructuresAndVariables,CrudAEOStructures);
+  deployer.link(StructuresAndVariables,CrudAEOCreateAndRead);
+
   deployer.deploy(CrudAEOStructures,{from: "0x1caFCe8fF232515Dcb07eE7901D49cFF8397eF7c"}, {overwrite: false}).then(function() {
   	return deployer.deploy(CrudAEOCreateAndRead, CrudAEOStructures.address, {from: "0x1caFCe8fF232515Dcb07eE7901D49cFF8397eF7c"});
-
-
-  });
+ });
 
 
 
